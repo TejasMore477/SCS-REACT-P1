23bbb4cb372001df71145ee9c8249d05ba888ebd
Tejas More <tejasmore660@gmail.com>
2024-05-10 23:23:51 +0530

    added key

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -25,7 +25,7 @@ function Songs(){
         <>
             <div className=" w-full h-screen bg-gray-900 flex flex-col items-center justify-center gap-2">
                 {data.map((element, index) => (
-                    <div className=" w-90 bg-slate-200 px-2 py-2 rounded">
+                    <div key={index} className=" w-90 bg-slate-200 px-2 py-2 rounded">
                         <h1 className=" font-medium text-lg mb-1">{element.name}</h1>
                         <p className=" font-light text-sm ">{element.description}</p>
                         <button onClick={downlodeFunctoin} className=" bg-blue-400 font-medium text-sm px-2 py-1 mt-2">Downlode</button>
